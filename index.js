cf7a0df05242ad0c147667b2e4a9d886863b8c77
naturemat1 <mateopaulna2@gmail.com>
2026-08-03 12:09:20 -0500

    Adding my README version with mustache

---
+++ index.js
@@ -0,0 +1,32 @@
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  name: 'Mateo',
+  title: 'Fullstack developer',
+  linkedin_url: 'https://www.linkedin.com/in/mateo-cobo-7847683b1/',
+  update_interval: '3 hours',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'America/Guayaquil', // Cambia a tu zona horaria
+  }),
+};
+
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+    console.log('✅ README.md generado correctamente');
+    console.log(`📅 Última actualización: ${DATA.date}`);
+  });
+}
+
+generateReadMe();
